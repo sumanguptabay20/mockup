d9bab26839ccf63c4841c879380e1f9694cb1134
sumanguptabay20 <139864287+sumanguptabay20@users.noreply.github.com>
2023-09-21 11:45:55 +0545

    Add files via upload

---
+++ swiper.js
@@ -0,0 +1,33 @@
+
+
+$('.demo-crausel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:2,
+    autoplay: true
+})
+
+$('.team-crausel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    items:2
+})
+
+$('.demo-1-crausel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    items:1,
+    autoplay: true
+})
+
+$('.team-crausel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    items:1
+})
